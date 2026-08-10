ad55835d269c3f01606fa055be5db64df83e4898
OzodbekX <ozodnx@gmail.com>
2026-08-10 09:06:27 +0500

    Add .gitignore and initial back/client files

---
+++ back/index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const mongoose = require("mongoose");
