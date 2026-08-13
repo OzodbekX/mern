fbbb8e60ba728553c46ae8518a1ac57a7addddbd
Ozodbek <78174455+OzodbekX@users.noreply.github.com>
2026-08-13 11:43:12 +0500

    add models

--- back/index.js
+++ back/index.js
@@ -1,6 +1,19 @@
+const sequelize = require("./db");
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db");
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+}
+start();
+
 // const mongoose = require("mongoose");
---
+++ back/models/model.js
@@ -0,0 +1,19 @@
+const sequlize = require("../db");
+const { DataTypes } = require("sequelize");
+const User = sequlize.define("user", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: "USER" }
+});
+const Basket = sequlize.define("basket", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+});
+const BasketDevice = sequlize.define("basket_device", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+});
+const Device = sequlize.define("device", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+    price: { type: DataTypes.INTEGER, allowNull: false },
+    
